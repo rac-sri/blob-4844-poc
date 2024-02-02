2d1b628a90cf7d7545585b73ad9bb2ce0d7b3c1a
rac-sri <rac.sri25@gmail.com>
2024-02-02 14:11:54 +0530

    feat: txs going through !

--- main.go
+++ main.go
@@ -49,22 +49,47 @@ func main() {
 		Proofs:      []kzg4844.Proof{emptyBlobProof},
 	}
 
+	ctx := context.Background()
+	client, _ := ethclient.DialContext(ctx, os.Getenv("NODE_URL"))
+
+	privateKey, _ := getECDSAPrivateKey()
+	publicKey := privateKey.PublicKey
+
+	nonce, err := client.PendingNonceAt(ctx, crypto.PubkeyToAddress(publicKey))
+	if err != nil {
+		log.Fatalf("Error getting nonce: %v", err)
+	}
+
+	head, err := client.HeaderByNumber(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	baseFee := head.BaseFee
+	suggestedTip, err := client.SuggestGasTipCap(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tip := new(big.Int).Add(suggestedTip, big.NewInt(10e9))
+	maxFeePerGas := new(big.Int).Add(baseFee, tip)
+	maxFeePerGas.Add(maxFeePerGas, big.NewInt(10e9))
+
 	chainId := uint256.MustFromBig(big.NewInt(5))
 
 	blobtx := types.NewTx(&types.BlobTx{
 		ChainID:    chainId,
-		Nonce:      5,
-		GasTipCap:  uint256.NewInt(2),
-		GasFeeCap:  uint256.NewInt(2),
-		Gas:        25000,
-		To:         common.HexToAddress("0x06fd9d0Ae9052A85989D0A30c60fB11753537f9A"),
-		Value:      uint256.NewInt(99),
+		Nonce:      nonce,
+		GasTipCap:  uint256.MustFromBig(tip),
+		GasFeeCap:  uint256.MustFromBig(maxFeePerGas),
+		Gas:        2500000,
+		To:         common.HexToAddress("0xF5106D4ef61cd0a04a345495f59f536bB7cd6074"),
+		Value:      uint256.NewInt(0),
 		Data:       make([]byte, 50),
 		BlobFeeCap: uint256.NewInt(15),
 		BlobHashes: sidecar.BlobHashes(),
+		Sidecar:    &sidecar,
 	})
-
-	privateKey, err := getECDSAPrivateKey()
 
 	signedTx, _ := types.SignTx(blobtx, types.NewCancunSigner(big.NewInt(5)), privateKey)
 
@@ -74,8 +99,6 @@ func main() {
 
 	rlpData, _ := signedTx.MarshalBinary()
 
-	ctx := context.Background()
-	client, err := ethclient.DialContext(ctx, os.Getenv("NODE_URL"))
 	if err != nil {
 		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
 	}
@@ -85,7 +108,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to send transaction: %v", err)
 	} else {
-		log.Printf("successfully sent transaction. txhash=%v", signedTx.Hash())
+		log.Printf("successfully sent transaction. txhash= %v", signedTx.Hash())
 	}
 
 }
@@ -105,82 +128,3 @@ func getECDSAPrivateKey() (*ecdsa.PrivateKey, error) {
 
 	return privateKey, nil
 }
-
-// func generateAndAppendSignatureValues(tx *types.BlobTx, encodedTx *[]byte, privateKey *ecdsa.PrivateKey) {
-// 	signature, err := crypto.Sign(crypto.Keccak256Hash(*encodedTx).Bytes(), privateKey)
-// 	if err != nil {
-// 		fmt.Println("error singing the encoded tx object")
-// 		return
-// 	}
-
-// 	r, overflow := uint256.FromBig(new(big.Int).SetBytes(signature[:32]))
-// 	if overflow {
-// 		return
-// 	}
-
-// 	s, overflow := uint256.FromBig(new(big.Int).SetBytes(signature[32:64]))
-// 	if overflow {
-// 		return
-// 	}
-
-// 	vByte := signature[64]
-// 	if vByte == 0 || vByte == 1 {
-// 		vByte += 27 // Adjust according to Ethereum's v value conventions
-// 	}
-// 	v := uint256.NewInt(uint64(vByte))
-
-// 	tx.Value = v
-// 	tx.R = r
-// 	tx.S = s
-// }
-
-// func generateRandomHashes(n int) [][]byte {
-// 	hashes := make([][]byte, n)
-// 	for i := 0; i < n; i++ {
-// 		hash := make([]byte, 32)
-// 		_, err := rand.Read(hash)
-// 		if err != nil {
-// 			panic("error generating random hash")
-// 		}
-// 		hashes[i] = hash
-// 	}
-// 	return hashes
-// }
-
-// func createAndSendTransaction(txHex string) {
-// 	payload := map[string]interface{}{
-// 		"jsonrpc": "2.0",
-// 		"method":  "eth_sendRawTransaction",
-// 		"params":  []interface{}{txHex},
-// 		"id":      5,
-// 	}
-// 	requestBody, err := json.Marshal(payload)
-// 	if err != nil {
-// 		fmt.Println("Error marshaling request:", err)
-// 		return
-// 	}
-
-// 	req, err := http.NewRequest("POST", os.Getenv("NODE_URL"), bytes.NewBuffer(requestBody))
-// 	if err != nil {
-// 		fmt.Println("Error creating request:", err)
-// 		return
-// 	}
-
-// 	req.Header.Set("Content-Type", "application/json")
-
-// 	client := &http.Client{}
-// 	resp, err := client.Do(req)
-// 	if err != nil {
-// 		fmt.Println("Error sending request:", err)
-// 		return
-// 	}
-// 	defer resp.Body.Close()
-
-// 	responseBody, err := io.ReadAll(resp.Body)
-// 	if err != nil {
-// 		fmt.Println("Error reading response body:", err)
-// 		return
-// 	}
-
-// 	fmt.Printf("Response: %s\n", responseBody)
-// }
